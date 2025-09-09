2f6a8972aced9e70b6d9dae6ded7f812780650ca
Eyef <dslnet2@yahoo.com>
2025-09-09 16:08:55 +0300

    Add files via upload
    Fixed critical errors in WAV file headers. Now files with ADPCM, XMA2 and Extensible formats open correctly in sound editors (GoldWave, etc.), thanks to: 
    Addition and correct filling of fact-chunk.
    Precise calculation of chunk sizes and number of samples.
    Format conformity to Microsoft WAVEFORMATEX and RIFF WAVE specifications.

--- ResourceFileEditor/Manager/Audio/AudioManager.cs
+++ ResourceFileEditor/Manager/Audio/AudioManager.cs
@@ -20,32 +20,53 @@ namespace ResourceFileEditor.Manager.Audio
             int index = 0;
             int extSize = 2;
 
-            UInt32 magic = FileManager.FileManager.readUint32Swapped(file, index);
-            if (magic == SOUND_MAGIC_IDMSA)
+            try
             {
-                setWaveHeader(data);
-
-                WaveFormat waveFormat = new WaveFormat();
+                /* ---------- magic ---------- */
+                UInt32 magic = FileManager.FileManager.readUint32Swapped(file, index);
+                if (magic != SOUND_MAGIC_IDMSA)
+                {
+                    data.SetLength(0);
+                    return data;
+                }
                 index += 4;
-                index += 8; // skip timestamp
-                index += 1; // skip loaded
+
+                /* ---------- RIFF header ---------- */
+                WriteRiffHeader(data);
+
+                /* ---------- fact chunk (placeholder) ---------- */
+                long factSamplesPos = data.Position + 8;
+                data.Write(Encoding.ASCII.GetBytes("fact"), 0, 4);
+                data.Write(BitConverter.GetBytes(4), 0, 4);
+                data.Write(BitConverter.GetBytes(0U), 0, 4);
+
+                /* ---------- fmt chunk ---------- */
+                long fmtSizePos = data.Position + 4;
+                data.Write(Encoding.ASCII.GetBytes("fmt "), 0, 4);
+                data.Write(BitConverter.GetBytes(0), 0, 4);
+                long fmtDataStart = data.Position;
+
+                index += 8;  // timestamp
+                index += 1;  // loaded flag
                 int playBegin = FileManager.FileManager.readIntSwapped(file, index);
                 index += 4;
                 int playLength = FileManager.FileManager.readIntSwapped(file, index);
                 index += 4;
 
+                /* ---------- WAVEFORMATEX ---------- */
                 byte[] fbr = FileManager.FileManager.readByteArray(file, index, WaveFormat.Basic.classSize);
-                data.Write(fbr, 0, WaveFormat.Basic.classSize);
                 index += WaveFormat.Basic.classSize;
+                data.Write(fbr, 0, fbr.Length);
+                var basic = WaveFormat.Basic.fromByteArray(fbr);
 
-                waveFormat.basic = WaveFormat.Basic.fromByteArray(fbr);
+                audioInfo.SampleRate = (int)basic.samplesPerSec;
+                audioInfo.Channels = basic.numChannels;
+                audioInfo.BitsPerSample = basic.bitsPerSample;
 
-                // Save the basic parameters
-                audioInfo.SampleRate = (int)waveFormat.basic.samplesPerSec;
-                audioInfo.Channels = waveFormat.basic.numChannels;
-                audioInfo.BitsPerSample = waveFormat.basic.bitsPerSample;
+                uint factSamples = 0;
 
-                switch(waveFormat.basic.formatTag)
+                /* ---------- extra fmt ---------- */
+                switch (basic.formatTag)
                 {
                     case (ushort)WaveFormat.Basic.FormatTag.FORMAT_PCM:
                         extSize = 0;
@@ -53,40 +74,40 @@ namespace ResourceFileEditor.Manager.Audio
                         break;
 
                     case (ushort)WaveFormat.Basic.FormatTag.FORMAT_ADPCM:
-                        data.Write(BitConverter.GetBytes((short)WaveFormat.Extra.Adpcm.classSize), 0, 2);
-                        waveFormat.extraSize = FileManager.FileManager.readUint16(file, index);
+                        audioInfo.Format = AudioFormat.ADPCM;
+                        ushort extraAdpcm = FileManager.FileManager.readUint16(file, index);
                         index += 2;
                         byte[] abr = FileManager.FileManager.readByteArray(file, index, WaveFormat.Extra.Adpcm.classSize);
-                        index += WaveFormat.Extra.Adpcm.classSize;
-                        data.Write(abr, 0, WaveFormat.Extra.Adpcm.classSize);
-                        extSize += WaveFormat.Extra.Adpcm.classSize;
-                        audioInfo.Format = AudioFormat.ADPCM;
+                        index += abr.Length;
+                        data.Write(BitConverter.GetBytes((short)WaveFormat.Extra.Adpcm.classSize), 0, 2);
+                        data.Write(abr, 0, abr.Length);
+                        extSize += abr.Length;
                         break;
 
                     case (ushort)WaveFormat.Basic.FormatTag.FORMAT_XMA2:
-                        data.Write(BitConverter.GetBytes((short)WaveFormat.Extra.Xma2.classSize), 0, 2);
-                        waveFormat.extraSize = FileManager.FileManager.readUint16(file, index);
+                        audioInfo.Format = AudioFormat.XMA2;
+                        ushort extraXma = FileManager.FileManager.readUint16(file, index);
                         index += 2;
                         byte[] xbr = FileManager.FileManager.readByteArray(file, index, WaveFormat.Extra.Xma2.classSize);
-                        index += WaveFormat.Extra.Xma2.classSize;
-                        data.Write(xbr, 0, WaveFormat.Extra.Xma2.classSize);
-                        extSize += WaveFormat.Extra.Xma2.classSize;
-
+                        index += xbr.Length;
+                        data.Write(BitConverter.GetBytes((short)WaveFormat.Extra.Xma2.classSize), 0, 2);
+                        data.Write(xbr, 0, xbr.Length);
+                        extSize += xbr.Length;
                         var xma2 = WaveFormat.Extra.Xma2.fromByteArray(xbr);
                         audioInfo.IsXma2 = true;
                         audioInfo.Xma2PlayLength = xma2.playLength;
-                        audioInfo.Format = AudioFormat.XMA2;
+                        factSamples = xma2.playLength;
                         break;
 
                     case (ushort)WaveFormat.Basic.FormatTag.FORMAT_EXTENSIBLE:
-                        data.Write(BitConverter.GetBytes((short)WaveFormat.Extra.Extensible.classSize), 0, 2);
-                        waveFormat.extraSize = FileManager.FileManager.readUint16(file, index);
+                        audioInfo.Format = AudioFormat.Extensible;
+                        ushort extraExt = FileManager.FileManager.readUint16(file, index);
                         index += 2;
                         byte[] ebr = FileManager.FileManager.readByteArray(file, index, WaveFormat.Extra.Extensible.classSize);
-                        index += WaveFormat.Extra.Extensible.classSize;
-                        data.Write(ebr, 0, WaveFormat.Extra.Extensible.classSize);
-                        extSize += WaveFormat.Extra.Extensible.classSize;
-                        audioInfo.Format = AudioFormat.Extensible;
+                        index += ebr.Length;
+                        data.Write(BitConverter.GetBytes((short)WaveFormat.Extra.Extensible.classSize), 0, 2);
+                        data.Write(ebr, 0, ebr.Length);
+                        extSize += ebr.Length;
                         break;
 
                     default:
@@ -94,63 +115,85 @@ namespace ResourceFileEditor.Manager.Audio
                         break;
                 }
 
-                data.Write(Encoding.ASCII.GetBytes("data"), 0, 4);
-                int amplitudeNum = FileManager.FileManager.readIntSwapped(file, index);
-                index += 4;
-                index += amplitudeNum; // skip amplitudes
+                /* ---------- correct fmt size ---------- */
+                int fmtChunkSize = (int)(data.Position - fmtDataStart);
+                data.Position = fmtSizePos;
+                data.Write(BitConverter.GetBytes(fmtChunkSize), 0, 4);
+                data.Position = data.Length;
 
+                /* ---------- amplitude skip ---------- */
+                int ampLen = FileManager.FileManager.readIntSwapped(file, index);
+                index += 4;
+                if (ampLen > 0) index += ampLen;
+
+                /* ---------- total buffer size ---------- */
                 int totalBufferSize = FileManager.FileManager.readIntSwapped(file, index);
                 index += 4;
                 audioInfo.DataSize = totalBufferSize;
 
-                data.Write(BitConverter.GetBytes(totalBufferSize), 0, 4);
-
                 int bufferNum = FileManager.FileManager.readIntSwapped(file, index);
                 index += 4;
+
                 int totalSamples = 0;
+                long audioStartInMem = data.Position;
+                data.Write(Encoding.ASCII.GetBytes("data"), 0, 4);
+                long dataSizePos = data.Position;
+                data.Write(BitConverter.GetBytes(0), 0, 4);
+                long audioDataStart = data.Position;
+
                 for (int i = 0; i < bufferNum; i++)
                 {
                     int numSamples = FileManager.FileManager.readIntSwapped(file, index);
                     index += 4;
-                    int bufferSize = FileManager.FileManager.readIntSwapped(file, index);
+                    int bufSize = FileManager.FileManager.readIntSwapped(file, index);
                     index += 4;
-                    byte[] buffer = FileManager.FileManager.readByteArray(file, index, bufferSize);
-                    index += bufferSize;
-                    if (buffer != null)
-                    {
-                        data.Write(buffer, 0, bufferSize);
-                        totalSamples += numSamples;
-                    }
+                    byte[] buf = FileManager.FileManager.readByteArray(file, index, bufSize);
+                    index += bufSize;
+                    data.Write(buf, 0, bufSize);
+                    totalSamples += numSamples;
                 }
-
-                // <-- Save totalSamples to audioInfo -->
                 audioInfo.TotalSamples = totalSamples;
 
-                if (!audioInfo.IsXma2)
+                /* ---------- fix for size of data chunk ---------- */
+                long realAudioSize = data.Position - audioDataStart;
+                data.Position = dataSizePos;
+                data.Write(BitConverter.GetBytes((int)realAudioSize), 0, 4);
+                data.Position = data.Length;
+
+                /* ---------- fix for fact ---------- */
+                if (audioInfo.Format == AudioFormat.ADPCM)
                 {
-                    audioInfo.DataSize = totalSamples * waveFormat.basic.numChannels * (waveFormat.basic.bitsPerSample / 8);
+                    ushort spb = (ushort)((basic.blockSize - 7 * basic.numChannels) * 2 + 2);
+                    uint blocks = (uint)(realAudioSize / basic.blockSize);
+                    factSamples = blocks * spb;
                 }
+                else if (audioInfo.Format == AudioFormat.Extensible)
+                    factSamples = (uint)totalSamples;
+
+                data.Position = factSamplesPos;
+                data.Write(BitConverter.GetBytes(factSamples), 0, 4);
+                data.Position = data.Length;
+
+                /* ---------- final RIFF size ---------- */
+                long riffSize = data.Length - 8;
+                data.Position = 4;
+                data.Write(BitConverter.GetBytes((int)riffSize), 0, 4);
+                data.Position = 0;
+
+                return data;
             }
-
-            data.Position = 4;
-            data.Write(BitConverter.GetBytes(data.Length - 8), 0, 4);
-            data.Position = 16;
-            data.Write(BitConverter.GetBytes(WaveFormat.Basic.classSize + extSize), 0, 4);
-            data.Position = 0;
-
-            return data;
+            catch
+            {
+                data.SetLength(0);
+                return data;
+            }
         }
 
-        private static void setWaveHeader(MemoryStream data)
+        private static void WriteRiffHeader(MemoryStream ms)
         {
-            byte[] buffer = Encoding.ASCII.GetBytes("RIFF");
-            data.Write(buffer, 0, 4);
-            data.Write(BitConverter.GetBytes(0), 0, 4); // Stream size TBF
-            buffer = Encoding.ASCII.GetBytes("WAVE");
-            data.Write(buffer, 0, 4);
-            buffer = Encoding.UTF8.GetBytes("fmt ");
-            data.Write(buffer, 0, 4);
-            data.Write(BitConverter.GetBytes(WaveFormat.Basic.classSize), 0, 4);
+            ms.Write(Encoding.ASCII.GetBytes("RIFF"), 0, 4);
+            ms.Write(BitConverter.GetBytes(0), 0, 4);
+            ms.Write(Encoding.ASCII.GetBytes("WAVE"), 0, 4);
         }
     }
 }
